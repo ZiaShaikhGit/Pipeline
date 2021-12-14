a255363878fdaec7be41784216f13c1752f404cd
Shaikh Ziauddin <shaikh.ziauddin@dxc.com>
2021-12-14 11:21:40 +0530

    commit

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace pipeline
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //New Configuration 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
